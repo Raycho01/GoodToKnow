84dccf84c5d5ade132dda99e9333729733ce37c6
Raycho Kostadinov <Raycho.Kostadinov@dskbank.bg>
2024-01-22 14:53:41 +0200

    Initial commit

---
+++ GoodToKnow/GoodToKnow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodToKnow
+//
+//  Created by Raycho Kostadinov on 22.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
