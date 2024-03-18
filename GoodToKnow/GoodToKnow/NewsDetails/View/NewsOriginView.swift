85df100ceaf0064a1957bdb15a6ac655cfb9179b
Raycho Kostadinov <Raycho.Kostadinov@dskbank.bg>
2024-03-18 22:19:25 +0200

    UI touches

--- GoodToKnow/GoodToKnow/NewsDetails/View/NewsOriginView.swift
+++ GoodToKnow/GoodToKnow/NewsDetails/View/NewsOriginView.swift
@@ -13,17 +13,17 @@ final class NewsOriginView: UIView {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.distribution = .fill
-        stackView.spacing = 10
+        stackView.spacing = 8
         stackView.backgroundColor = .clear
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
-    private let author: String?
-    private let source: String?
-    private let publishedAt: String?
+    private let author: String
+    private let source: String
+    private let publishedAt: String
     
-    init(frame: CGRect, author: String?, source: String?, publishedAt: String?) {
+    init(frame: CGRect, author: String, source: String, publishedAt: String) {
         self.author = author
         self.source = source
         self.publishedAt = publishedAt
@@ -41,20 +41,12 @@ final class NewsOriginView: UIView {
         addSubview(vStackView)
         vStackView.fillSuperview(padding: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
         
-        if let author = author {
-            addRow(icon: "person.fill", firstText: "Author:", secondText: author)
-        }
-        
-        if let source = source {
-            addRow(icon: "link.circle", firstText: "Source:", secondText: source)
-        }
-        
-        if let publishedAt = publishedAt {
-            addRow(icon: "calendar", firstText: "Published at:", secondText: publishedAt)
-        }
+        addRow(icon: "person.fill", firstText: "Author:", secondText: author, separator: true)
+        addRow(icon: "link.circle", firstText: "Source:", secondText: source, separator: true)
+        addRow(icon: "calendar", firstText: "Published at:", secondText: publishedAt)
     }
     
-    private func addRow(icon: String, firstText: String, secondText: String) {
+    private func addRow(icon: String, firstText: String, secondText: String, separator: Bool = false) {
         let rowView = UIStackView()
         rowView.axis = .horizontal
         rowView.spacing = 10
@@ -62,17 +54,28 @@ final class NewsOriginView: UIView {
         let iconImageView = UIImageView(image: UIImage(systemName: icon))
         iconImageView.contentMode = .scaleAspectFit
         iconImageView.tintColor = .black
+        iconImageView.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         rowView.addArrangedSubview(iconImageView)
         
         let firstLabel = UILabel()
         firstLabel.text = firstText
+        firstLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         rowView.addArrangedSubview(firstLabel)
         
         let secondLabel = UILabel()
         secondLabel.text = secondText
         secondLabel.textColor = UIColor.MainColors.secondaryText
+        secondLabel.font = .systemFont(ofSize: 14)
+        secondLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
         rowView.addArrangedSubview(secondLabel)
         
         vStackView.addArrangedSubview(rowView)
+        
+        guard separator else { return }
+        let separator = UIView()
+        separator.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        separator.backgroundColor = .black.withAlphaComponent(0.1)
+        vStackView.addArrangedSubview(separator)
+        separator.widthAnchor.constraint(equalTo: vStackView.widthAnchor, multiplier: 0.98).isActive = true
     }
 }
